c953a7ed8a9b000d67ba05d9a3283f24fe669ebc
AyrielNoriega <ayriel.rentek@rentek.com.co>
2024-01-09 14:50:35 -0500

    add register page

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,5 +1,92 @@
+import { Link as LinkRouterDom } from 'react-router-dom';
+import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { AuthLayout } from '../layout/AuthLayout';
+
 export const RegisterPage = () => {
     return (
-        <div>RegisterPage</div>
+        <AuthLayout title="Register">
+            <form>
+                <Grid container>
+                    <Grid
+                        item
+                        xs={ 12 }
+                        sx={{
+                            mt: 1
+                        }}
+                    >
+                        <TextField
+                            label="Nombre completo"
+                            type="text"
+                            placeholder="Nombre completo"
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid
+                        item
+                        xs={ 12 }
+                        sx={{
+                            mt: 1
+                        }}
+                    >
+                        <TextField
+                            label="Correo"
+                            type="email"
+                            placeholder="correo@gmail.com"
+                            fullWidth
+                        />
+                    </Grid>
+
+                    <Grid
+                        item
+                        xs={ 12 }
+                        sx={{
+                            mt: 1
+                        }}
+                    >
+                        <TextField
+                            label="Contraseña"
+                            type="password"
+                            placeholder="Contraseña"
+                            fullWidth
+                        />
+                    </Grid>
+
+                    <Grid
+                        container
+                        spacing={ 2 }
+                        sx={{
+                            mb: 2,
+                            mt: 1
+                        }}
+                    >
+                        <Grid
+                            item
+                            xs={ 12 }
+                            sm={ 12 }
+                        >
+                            <Button variant="contained" fullWidth>
+                                Crear cuenta
+                            </Button>
+                        </Grid>
+
+                    </Grid>
+
+                    <Grid
+                        container
+                        direction="row"
+                        justifyContent="end"
+                    >
+                        <Typography sx={{ mr: 1 }}>¿Ya tiene  una cuenta ?</Typography>
+                        <Link
+                            component={ LinkRouterDom }
+                            color="inherit"
+                            to="/auth/login"
+                        >
+                            Ingresar
+                        </Link>
+                    </Grid>
+                </Grid>
+            </form>
+        </AuthLayout>
     );
 };
