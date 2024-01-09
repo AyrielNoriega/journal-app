c8b5bad071ba5ab68333fd5cfe2a5a2d51caa692
AyrielNoriega <ayriel.rentek@rentek.com.co>
2024-01-09 12:45:05 -0500

    config materuial ui con vite

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,5 +1,10 @@
+import { AppRouter } from './router/AppRouter';
+import { AppTheme } from './theme';
+
 export const JournalApp = () => {
     return (
-        <div>JournalApp</div>
+        <AppTheme>
+            <AppRouter />
+        </AppTheme>
     );
 };
