af0f3cf0ea790a0c3f3558c15197ada45724b93d
AyrielNoriega <ayriel.rentek@rentek.com.co>
2024-01-11 13:29:07 -0500

    290. Checking Authentication

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,18 @@
+import { useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
+
 import { AuthRoutes } from '../auth/routes/AuthRoutes';
 import { JournalRoutes } from '../journal/routes/JournalRoutes';
+import { CheckingAuth } from '../ui';
 
 export const AppRouter = () => {
+    const { status } = useSelector(state => state.auth);
+    console.log('app router');
+    console.log(status);
+    if (status === 'checking') {
+        return <CheckingAuth />;
+    }
+
     return (
         <Routes>
             <Route path="/auth/*" element={ <AuthRoutes /> } />
