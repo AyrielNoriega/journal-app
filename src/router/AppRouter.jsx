0907b7f251ade71c31cd939109620eee294045f7
AyrielNoriega <ayriel.rentek@rentek.com.co>
2024-01-12 09:34:07 -0500

    291. Mantener el estado de la autenticación al recargar

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,22 +1,26 @@
-import { useSelector } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 import { AuthRoutes } from '../auth/routes/AuthRoutes';
 import { JournalRoutes } from '../journal/routes/JournalRoutes';
 import { CheckingAuth } from '../ui';
+import { useCheckOut } from '../hooks';
 
 export const AppRouter = () => {
-    const { status } = useSelector(state => state.auth);
-    console.log('app router');
-    console.log(status);
+    const { status } = useCheckOut();
     if (status === 'checking') {
         return <CheckingAuth />;
     }
 
     return (
         <Routes>
-            <Route path="/auth/*" element={ <AuthRoutes /> } />
-            <Route path="/*" element={ <JournalRoutes /> } />
+            {
+                (status === 'authenticated')
+                    ? <Route path="/*" element={ <JournalRoutes /> } />
+                    : <Route path="/auth/*" element={ <AuthRoutes /> } />
+            }
+
+            <Route path="/*" element={ <Navigate to="/auth/login" /> } />
+
         </Routes>
     );
 };
