bb175e586e95b17d2159a8a50c3507ebe3da47c6
AyrielNoriega <anoriega2310@gmail.com>
2024-01-19 20:13:19 -0500

    307. Actualizar la nota actual

--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -30,14 +30,20 @@ export const journalSlice = createSlice({
             state.active = action.payload;
         },
         setNotes: (state, action) => {
-            console.log(action);
             state.notes = action.payload;
         },
         setSaving: (state) => {
-
+            state.isSaving = true;
         },
         updateNote: (state, action) => {
+            state.isSaving = false;
+            state.notes = state.notes.map(note => {
+                if (note.id === action.payload.id) {
+                    return action.payload;
+                }
 
+                return note;
+            });
         },
         deleteNoteById: (state, action) => {
 
--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,6 +1,6 @@
 import { collection, doc, setDoc } from 'firebase/firestore/lite';
 import { FirebaseDB } from '../../firebase/config';
-import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes } from './journalSlice';
+import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes, setSaving, updateNote } from './journalSlice';
 import { loadNotes } from '../../helpers/loadNotes';
 
 export const startNewNote = () => {
@@ -32,5 +32,21 @@ export const startLoadingNote = () => {
         const notes = await loadNotes(uid);
         dispatch(setNotes(notes));
         console.log(notes);
+    };
+};
+
+export const startSaveNote = () => {
+    return async (dispatch, getState) => {
+        dispatch(setSaving());
+
+        const { uid } = getState().auth;
+        const { active: note } = getState().journal;
+
+        const noteToFireStore = { ...note };
+        delete noteToFireStore.id;
+
+        const docRef = doc(FirebaseDB, `${uid}/journal/notes/${note.id}`);
+        await setDoc(docRef, noteToFireStore, { merge: true });
+        dispatch(updateNote(note));
     };
 };
